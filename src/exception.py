9552484fd50cd7ff93bfe8910acbcb9be9b28c25
Karnsinh Chikane <karnsinh96@gmail.com>
2024-01-16 22:26:25 +0530

    Folders Added

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+'''SHOWING ERROR DETAILS'''
+
+import sys
+
+def error_message_details(error,error_details:sys):
+    _,_,exc_tb=error_details.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+
+    error_message=''' Error Found in Python File [{0}],
+                      Line Number [{1}],
+                      Error Message [{2}]
+                    '''.format(file_name,exc_tb.tb_lineno,str(error))
+    
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_details(error_message,error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
