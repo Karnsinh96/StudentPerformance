419be342a7b4859a38b558cb2196fd3aafec970b
Karnsinh Chikane <karnsinh96@gmail.com>
2024-01-17 19:06:44 +0530

    Data Ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformaton import DataTransformation, DataTransformationConfig
 from src.components.model_trainer import ModelTrainer
+
+
 @dataclass
 class DataIngestionConfig:
 
@@ -23,10 +25,10 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('stud.csv')
+            df = pd.read_csv('notebooks\stud.csv')
             logging.info('Read the dataset as a dataframe')
 
-            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
@@ -37,7 +39,7 @@ class DataIngestion:
 
             logging.info("Ingestion of the data is completed")
 
-            return (
+            return ( 
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
@@ -46,10 +48,10 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-
+    obj.initiate_data_ingestion()
+'''
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
     model_trainer = ModelTrainer()
-    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))'''
