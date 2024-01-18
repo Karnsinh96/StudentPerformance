9669b69aacf5f7df871d34bd75456e3ca57bbd9b
Karnsinh96 <91832296+Karnsinh96@users.noreply.github.com>
2024-01-18 16:32:33 +0530

    Update app.py
    Checking CICD

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 import numpy as np
 import pandas as pd
-
+#Hello Checking CICD
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
